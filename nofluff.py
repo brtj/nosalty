655ea6ac33519c3ccb00e80a35fb7ee32fb2ae04
Bartosz Jarocki <bjarocki@egnyte.com>
2019-05-06 20:37:56 +0200

    Send to API

--- nofluff.py
+++ nofluff.py
@@ -6,6 +6,7 @@ import json
 import logging
 import argparse
 import configparser
+import requests
 from collections import namedtuple
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -28,12 +29,16 @@ class scraper_nofluff:
         config = configparser.ConfigParser()
         config.read('config')
         if 'nofluffscraper' in config:
-            noconf = namedtuple('config', ['retries', 'wait', 'sleep', 'scrolldown', 'timeout'])
+            noconf = namedtuple('config', ['retries', 'wait', 'sleep', 'scrolldown', 'timeout', 'url', 'api_login',
+                                           'api_pass'])
             nofluffconf = noconf(config['nofluffscraper']['retries'],
                                  config['nofluffscraper']['wait'],
                                  config['nofluffscraper']['sleep'],
                                  config['nofluffscraper']['scrolldown'],
-                                 config['nofluffscraper']['timeout'])
+                                 config['nofluffscraper']['timeout'],
+                                 config['django-server']['url'],
+                                 config['django-server']['api_login'],
+                                 config['django-server']['api_pass'])
             return nofluffconf
         else:
             raise ValueError('No config file created. Should be in script dir. Use config.template')
@@ -64,9 +69,6 @@ class scraper_nofluff:
 
 
     def url_get_offers(self, city, category):
-        """
-        Get offers and return offers list
-        """
         logging.info('Getting offers list for category %s in city %s.' % (category, city))
         category_list = self.url_get_categories_by_city(city)
         for url in category_list:
@@ -89,17 +91,8 @@ class scraper_nofluff:
         logging.info('Parsing offers list from %s in category: %s' % (city, category))
         timestamp = self.current_date()
         data = {'timestamp': timestamp, 'city': city, 'category': category, 'offers_count': len(offers_list)}
-        json_data = json.dumps(data)
         for url_offer in offers_list:
             self.parse_nofluffjob_offer(url_offer, city, category)
-
-        '''sd
-        parsowanie listy
-        data parsowania listy | miasto | ilosc ogloszen_lacznie | min wynagrodzenie | srednie | mediana | max
-
-        kolejna baza parsowanie listy
-        data parsowania listy | miasto | kategoria | ilosc ogloszen_kategorii | min wynagrodzenie | srednie | mediana | max
-        '''
 
 
     def parse_nofluffjob_offer(self, url, city, category):
@@ -110,16 +103,12 @@ class scraper_nofluff:
         vacancy_name = self.parse_get_field(offer, '.article-header-container', 'h1')
         company_name = self.parse_get_field(offer, '.dl-horizontal', 'dd')
         salary_uop_min, salary_uop_max, salary_b2b_min, salary_b2b_max = self.parse_get_salary(offer)
-        print('Vacancy name: %s' % vacancy_name)
-        print('Company name: %s' % company_name)
-        print('Salary UoP - min: %s PLN, max: %s PLN' % (salary_uop_min, salary_uop_max))
-        print('Salary B2B - min: %s PLN, max: %s PLN' % (salary_b2b_min, salary_b2b_max))
         data = {'timestamp': timestamp, 'vacancy_name': vacancy_name, 'company_name': company_name,
                         'city':city, 'category': category,
                         'salary_uop_min':salary_uop_min, 'salary_uop_max': salary_uop_max, 'salary_b2b_min': salary_b2b_min,
                         'salary_b2b_max': salary_b2b_max}
         json_data = json.dumps(data)
-        print(json_data)
+        self.send_to_API(data)
 
     def current_date(self):
         return datetime.datetime.now().strftime('%Y%m%d%H%M%S.%f')
@@ -138,11 +127,8 @@ class scraper_nofluff:
         for remove_string in remove_list:
             if remove_string in salary_text:
                 salary_text = salary_text.replace(remove_string,'')
-        #print(salary_text)
-        salary_uop = None
         salary_uop_min = None
         salary_uop_max = None
-        salary_b2b = None
         salary_b2b_min = None
         salary_b2b_max = None
         uop_string = '(UoP) per'
@@ -232,8 +218,16 @@ class scraper_nofluff:
         return salary
 
 
-    def send_to_API(self,query, json):
-        print('send to API - django server')
+    def send_to_API(self, json):
+        config = self.configinfo()
+        logging.info('Sending json to server')
+        login = config.api_login
+        password = config.api_pass
+        url = config.url
+        s = requests.Session()
+        s.auth = (login, password)
+        data = s.post(url, json=json)
+        logging.info('Response: %s, data: %s' % (data.status_code, data.text))
 
 
 def main():
@@ -242,7 +236,6 @@ def main():
     print(args)
     scraper.configinfo()
     offers_list = scraper.url_get_offers(args.city, args.category)
-    #offers_list = ['https://nofluffjobs.com/job/junior-ror-developer-netguru-70bca42w?criteria=category%253Dbackend%20city%253Dpozna%25C5%2584%20pozna%25C5%2584','https://nofluffjobs.com/job/senior-net-developer-next-it-poland-6e75navj?criteria=category%253Dbackend%20city%253Dpozna%25C5%2584%20pozna%25C5%2584','https://nofluffjobs.com/job/senior-java-developer-espeo-software-s5mitexi?criteria=category%253Dbackend%20city%253Dpozna%25C5%2584%20pozna%25C5%2584']
     scraper.parse_noflufjob_offers_list(offers_list, args.city, args.category)
 
 
