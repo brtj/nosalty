bceba1e54385616bda0edf3448d5e40c79ad9133
Bartosz Jarocki <bjarocki@egnyte.com>
2019-05-07 09:14:43 +0200

    Admin table

--- django-server/nosalty/nofluff/admin.py
+++ django-server/nosalty/nofluff/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Nofluff_data
 
-admin.site.register(Nofluff_data )
+
+class Nofluff_dataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vacancy_name', 'company_name', 'city', 'category')
+
+
+admin.site.register(Nofluff_data, Nofluff_dataAdmin)
