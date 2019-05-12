e15be05316fa6bc4884fb483ca3b209fd89aca90
Bartosz Jarocki <bjarocki@egnyte.com>
2019-05-13 00:15:37 +0200

    hide URL to offer

--- django-server/nosalty/main/tables.py
+++ django-server/nosalty/main/tables.py
@@ -2,13 +2,13 @@ import django_tables2 as tables
 from data_api.models import DataAggregator
 
 class AdsDataTable(tables.Table):
-    vacancy_name = tables.Column(attrs={'td': {'class': 'text-center'}}, verbose_name='Position')
+    vacancy_name = tables.Column(attrs={'td': {'class': 'text-left'}}, verbose_name='Position')
     company_name = tables.Column(verbose_name='Company Name')
     salary_uop_min = tables.Column(verbose_name='UoP min')
     salary_uop_max = tables.Column(verbose_name='UoP max')
     salary_b2b_min = tables.Column(verbose_name='B2B min')
     salary_b2b_max = tables.Column(verbose_name='B2B max')
-    url_to_offer = tables.TemplateColumn('<a href="{{record.url_to_offer}}">GO TO</a>')
+    #url_to_offer = tables.TemplateColumn('<a href="{{record.url_to_offer}}">GO TO</a>')
 
 
     class Meta:
@@ -20,6 +20,7 @@ class AdsDataTable(tables.Table):
             'salary_uop_min',
             'salary_uop_max',
             'salary_b2b_min',
-            'salary_b2b_max'
+            'salary_b2b_max',
+            #'url_to_offer'
         ]
         template_name = 'django_tables2/bootstrap4.html'
