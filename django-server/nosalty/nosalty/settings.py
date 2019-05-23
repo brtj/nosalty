f957b797103202659c1a6bdb5f159f8773265475
Bartosz Jarocki <bjarocki@egnyte.com>
2019-05-23 13:41:15 +0200

    statics file

--- django-server/nosalty/nosalty/settings.py
+++ django-server/nosalty/nosalty/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',)
