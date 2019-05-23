b8f999e7e729dd7139423ccf40a582f627ed4f0a
strzalkapsnid@gmail.com <strzalkapsnid@gmail.com>
2019-05-23 17:58:33 +0200

    add filters to admin site

--- django-server/nosalty/data_api/admin.py
+++ django-server/nosalty/data_api/admin.py
@@ -6,6 +6,11 @@ class DataAggregatorAdmin(admin.ModelAdmin):
     list_display = ('id', 'timestamp', 'vacancy_name', 'company_name', 'city', 'category')
     readonly_fields = ('timestamp',)
     ordering = ('-timestamp',)
+    list_filter = (
+        'city',
+        'category',
+    )
+    list_per_page = 200
 
 
 admin.site.register(DataAggregator, DataAggregatorAdmin)
